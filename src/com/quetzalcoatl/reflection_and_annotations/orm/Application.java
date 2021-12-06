35f6a331b4ea692cdd2bd655841634313e45d79a
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-12-06 22:57:15 +0300

    pseudo-hibernate: add read operation

--- src/com/quetzalcoatl/reflection_and_annotations/orm/Application.java
+++ src/com/quetzalcoatl/reflection_and_annotations/orm/Application.java
@@ -1,16 +1,17 @@
 package com.quetzalcoatl.reflection_and_annotations.orm;
 
-import java.sql.SQLException;
-
 public class Application {
-    public static void main(String[] args) throws SQLException, IllegalAccessException {
+    public static void main(String[] args) throws Exception {
         TransactionHistory jackHistory = new TransactionHistory(991377, "Jack", "Credit", 231);
         TransactionHistory samanthaHistory = new TransactionHistory(994873, "Samantha", "Debit", 44);
         TransactionHistory wotanHistory = new TransactionHistory(177354, "Wotan", "Credit", 236);
 
         Hibernate<TransactionHistory> hibernate = Hibernate.getConnection();
-        hibernate.write(jackHistory);
-        hibernate.write(samanthaHistory);
-        hibernate.write(wotanHistory);
+//        hibernate.write(jackHistory);
+//        hibernate.write(samanthaHistory);
+//        hibernate.write(wotanHistory);
+
+        TransactionHistory result = hibernate.read(TransactionHistory.class, 2L);
+        System.out.println(result);
     }
 }
--- src/com/quetzalcoatl/reflection_and_annotations/orm/Hibernate.java
+++ src/com/quetzalcoatl/reflection_and_annotations/orm/Hibernate.java
@@ -4,10 +4,8 @@ import com.quetzalcoatl.reflection_and_annotations.orm.annotations.Column;
 import com.quetzalcoatl.reflection_and_annotations.orm.annotations.PrimaryKey;
 
 import java.lang.reflect.Field;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringJoiner;
@@ -71,5 +69,44 @@ public class Hibernate<T> {
             }
         }
         preparedStatement.execute();
+    }
+
+    public T read(Class<T> clazz, long id) throws SQLException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Field[] declaredFields = clazz.getDeclaredFields();
+        Field pKey = null;
+        for(Field field : declaredFields){
+            if(field.isAnnotationPresent(PrimaryKey.class)){
+                pKey = field;
+                break;
+            }
+        }
+        if(pKey == null){
+            throw new RuntimeException("Can't find Primary key for the entity: " + clazz.getSimpleName());
+        }
+        String sql = String.format("SELECT * FROM %s WHERE %s=%s", clazz.getSimpleName(), pKey.getName(), id);
+        PreparedStatement preparedStatement = con.prepareStatement(sql);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        // cursor is initially positioned before the first row; the first call to the method next
+        // makes the first row the current row
+        resultSet.next();
+        T result = clazz.getConstructor().newInstance();
+
+        // set id to object
+        long transactionId = resultSet.getInt(pKey.getName());
+        pKey.setAccessible(true);
+        pKey.set(result, transactionId);
+
+        // set other fields to object
+        for(Field field : declaredFields){
+            if(field.isAnnotationPresent(Column.class)){
+                field.setAccessible(true);
+                if(field.getType() == Integer.class){
+                    field.set(result, resultSet.getInt(field.getName()));
+                }else if(field.getType() == String.class){
+                    field.set(result, resultSet.getString(field.getName()));
+                }
+            }
+        }
+        return result;
     }
 }
